74fb5fcb26c0bdfb83934a3295b02cd0bd8c3dcc
dkozak <dkozak94@gmail.com>
2019-03-26 23:35:27 +0000

    add example with working tooltip that stays if you hover over it and has a working button inside

--- src/tooltip/index.tsx
+++ src/tooltip/index.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import * as ReactTooltip from 'react-tooltip';
 
+import './styles.css';
 
-const Tooltip = () => <div>
-    <h3>Example with tooltip</h3>
-    <a data-tip="React-tooltip"> ◕‿‿◕ </a>
+const Tooltip = () => {
+    return <div>
+        <h3>Example with tooltip</h3>
+        This <a data-tip={true} data-for="my-tooltip"> <b>tooltip</b> </a> will stay there if you hover over it.
+        And buttons inside are working as well.
 
-    <ReactTooltip place="top" type="dark" effect="float">
-        <h1>hello!</h1>
-        <p>foo</p>
-        <h3>bar</h3>
-    </ReactTooltip>
+        <ReactTooltip className="extraClass" delayHide={500} id="my-tooltip" place="top" type="dark">
+            <h1>hello!</h1>
+            <p>foo</p>
+            <h3>bar</h3>
+            <button onClick={() => console.log('click')}>Click me</button>
+        </ReactTooltip>
 
-</div>;
+    </div>;
+};
 
 export default Tooltip;
