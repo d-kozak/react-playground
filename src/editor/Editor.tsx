7d3a4414b1d692baeabdd8db85870b17d69559ec
dkozak <dkozak94@gmail.com>
2019-03-27 20:55:18 +0000

    add simple cat dog highlighting -> it is working :)

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,8 +1,27 @@
 import * as React from "react";
 import {useState} from "react";
 import Editor from 'react-simple-code-editor';
+import {analyzeTokens, lexer} from "../tooltip/Lexer";
 
 const defaultCode = `A cat and a dog are friends`;
+
+
+const processCode = (code: string) => {
+
+    const tokens = analyzeTokens(lexer(code), new Set(['dog', 'cat']));
+
+    const processedHtml = tokens.map((token) => {
+        switch (token.type) {
+            case "text":
+            case "whitespace":
+                return token.value;
+            case "highlight":
+                return `<b>${token.value}</b>`
+        }
+    }).reduce((left, right) => left + right);
+
+    return processedHtml;
+};
 
 export default function EditorComponent() {
     const [code, setCode] = useState(defaultCode);
@@ -12,7 +31,7 @@ export default function EditorComponent() {
         <Editor
             value={code}
             onValueChange={code => setCode(code)}
-            highlight={code => code}
+            highlight={code => processCode(code)}
             padding={10}
             style={{
                 fontFamily: '"Fira code", "Fira Mono", monospace',
