6ee9fd275519adbf0f162a49b8a94a8bf0aacade
dkozak <dkozak94@gmail.com>
2019-03-27 20:51:49 +0000

    put empty react-simple-code-editor inside

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,9 +1,24 @@
-import * as React from 'react';
+import * as React from "react";
+import {useState} from "react";
+import Editor from 'react-simple-code-editor';
 
+const defaultCode = `A cat and a dog are friends`;
 
 export default function EditorComponent() {
+    const [code, setCode] = useState(defaultCode);
 
     return <div>
-        <h1>Editor here</h1>
+        <h1>Editor</h1>
+        <Editor
+            value={code}
+            onValueChange={code => setCode(code)}
+            highlight={code => code}
+            padding={10}
+            style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 12,
+            }}
+        />
+
     </div>
 }
