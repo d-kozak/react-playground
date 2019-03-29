efa9ce0a15d19265f3a4ed3701f0ea3756fe6ed2
dkozak <dkozak94@gmail.com>
2019-03-29 16:49:47 +0000

    finish the editor with highliting, it is possible to change the highlighting in settings, changes are 'effectively' immediate

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,8 +1,9 @@
-import {ErrorMessage, Field, FieldArray, Form, Formik} from "formik";
+import {ErrorMessage, FieldArray, Form, Formik} from "formik";
 import * as React from "react";
 import {Dispatch, SetStateAction, useState} from "react";
 import Editor from 'react-simple-code-editor';
 import * as Yup from 'yup';
+import StateDebugger from "../formik/StateDebugger";
 import {analyzeTokens, lexer} from "../tooltip/Lexer";
 
 /**
@@ -10,7 +11,6 @@ import {analyzeTokens, lexer} from "../tooltip/Lexer";
  */
 
 const defaultCode = `A cat and a dog are friends`;
-
 
 const HighlighForm = ({wordsToHighlight, setWordsToHighlight}: { wordsToHighlight: Array<string>, setWordsToHighlight: Dispatch<SetStateAction<Array<string>>> }) => {
     return <div>
@@ -26,18 +26,31 @@ const HighlighForm = ({wordsToHighlight, setWordsToHighlight}: { wordsToHighligh
                     Yup.string().required('Empty words not allowed')
                 )
             })}
-            render={({values, isSubmitting}) => <Form>
+            render={({values, submitForm, handleChange, handleBlur, isValid}) => <Form>
                 <FieldArray name="wordsToHighlight" render={({push, remove}) => <div>
                     {values.wordsToHighlight.map((word, index) => <div key={index}>
-                        <Field name={`wordsToHighlight.${index}`}/>
+                        <input type="text" name={`wordsToHighlight.${index}`} value={word} onChange={(e) => {
+                            handleChange(e);
+                            setTimeout(submitForm, 100)
+                        }} onBlur={(e) => {
+                            handleBlur(e);
+                            submitForm();
+                        }}/>
                         <ErrorMessage name={`wordsToHighlight.${index}`}/>
-                        <button type="button" onClick={() => remove(index)}>-</button>
+                        <button type="button" onClick={() => {
+                            remove(index);
+                            setTimeout(submitForm, 100)
+                        }}>-
+                        </button>
                     </div>)
                     }
                     <button type="button" onClick={() => push('')}>Add new</button>
+                    <StateDebugger state={{
+                        ...values,
+                        isValid
+                    }} title="Form state"/>
                 </div>
                 }/>
-                <button type="submit" disabled={isSubmitting}>Submit</button>
             </Form>
             }
         />
@@ -68,6 +81,19 @@ export default function EditorComponent() {
 
     return <div>
         <h1>Editor with highlighting</h1>
+        <p>This editor automatically bolds specified set a words. You can change this set in the settings.</p>
+        <p>The editor is implemented using react-simple-code-editor and the form is implemented using Formik.</p>
+        <p>To make the 'autosubmit' (the changes in the form are immeditally propagated to the state of the editor),
+            I ended up using normal input tag instead of Formik's Field component and also I had to use setTimeout to
+            execute
+            the submitForm() function. For some reason it did not work when I fired the form submission immediatelly
+            after
+            firing handleChange() or remove()...Maybe because of some asynchronous validation? :X</p>
+        <p>Maybe this is one of the use cases when using Formik is not beneficial and it would have been easier to just
+            write
+            all the form handling myself. In this example, it might have been easier. Nevertheless I consider Formik a
+            great
+            library for most forms.</p>
         <HighlighForm wordsToHighlight={wordsToHighlight} setWordsToHighlight={setWordsToHighlight}/>
         <h3>Text editor</h3>
         <Editor
@@ -78,8 +104,10 @@ export default function EditorComponent() {
             style={{
                 fontFamily: '"Fira code", "Fira Mono", monospace',
                 fontSize: 12,
+                backgroundColor: "#fff",
+                border: "solid 1px"
             }}
         />
-
+        <StateDebugger state={wordsToHighlight} title="Editor state"/>
     </div>
 }
