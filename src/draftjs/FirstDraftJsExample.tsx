651fab1dc9868550e89e45e35467c526d9ce4d95
dkozak <dkozak94@gmail.com>
2019-03-31 00:15:04 +0000

    refactor from hooks to classes

--- src/draftjs/FirstDraftJsExample.tsx
+++ src/draftjs/FirstDraftJsExample.tsx
@@ -1,44 +1,55 @@
 import * as Draft from "draft-js";
 import * as React from "react";
-import {CSSProperties, useState} from "react";
+import {CSSProperties} from "react";
 import StateDebugger from "../utils/StateDebugger";
+
 
 const styles: CSSProperties = {
     border: "solid 1px"
 };
 
-const regexStrategy = (block: Draft.ContentBlock, callback: (start: number, end: number) => void) => {
-    const text = block.getText();
-    let result: RegExpExecArray;
-    let regex = /(^|\s)#\w+/g;
-    while ((result = regex.exec(text) as RegExpExecArray) != null) {
-        let start = result.index;
-        let end = start + result[0].length;
-        callback(start, end);
-    }
-};
-
 // const regexComponent = (props:any) => <span style={{ backgroundColor: "lightgreen" }}>{props.children}</span>;
 const regexComponent = (props: any) => <b>{props.children}</b>;
 
-const compositeDecorator = new Draft.CompositeDecorator([
-    {
-        strategy: regexStrategy,
-        component: regexComponent
-    }
-]);
+export interface State {
+    editorState: Draft.EditorState
+}
 
-const FirstDraftJsExample = () => {
-    const [editorState, setEditorState] = useState(Draft.EditorState.createEmpty(compositeDecorator));
+export default class FirstDraftJsExample extends React.Component<{}, State> {
 
+    constructor(props: any) {
+        super(props);
+        const compositeDecorator = new Draft.CompositeDecorator([
+            {
+                strategy: this.regexStrategy,
+                component: regexComponent
+            }
+        ]);
+        this.state = {
+            editorState: Draft.EditorState.createEmpty(compositeDecorator)
+        };
+    };
 
-    return <div>
-        <h1>DraftJs</h1>
-        <div style={styles}>
-            <Draft.Editor editorState={editorState} onChange={setEditorState}/>
+    regexStrategy = (block: Draft.ContentBlock, callback: (start: number, end: number) => void) => {
+        const text = block.getText();
+        let result: RegExpExecArray;
+        let regex = /(^|\s)#\w+/g;
+        while ((result = regex.exec(text) as RegExpExecArray) != null) {
+            let start = result.index;
+            let end = start + result[0].length;
+            callback(start, end);
+        }
+    };
+
+    setEditorState = (newState: Draft.EditorState) => this.setState({editorState: newState});
+
+    render() {
+        return <div>
+            <h1>DraftJs</h1>
+            <div style={styles}>
+                <Draft.Editor editorState={this.state.editorState} onChange={this.setEditorState}/>
+            </div>
+            <StateDebugger state={this.state} title="Editor state"/>
         </div>
-        <StateDebugger state={editorState} title="Editor state"/>
-    </div>
+    }
 };
-
-export default FirstDraftJsExample;
