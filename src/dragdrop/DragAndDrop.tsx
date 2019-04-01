cf3b6e669e669ed012aeac81304397b32f90c225
dkozak <dkozak94@gmail.com>
2019-04-01 18:12:58 +0100

    add some styling to the drag and drop example

--- src/dragdrop/DragAndDrop.tsx
+++ src/dragdrop/DragAndDrop.tsx
@@ -5,7 +5,14 @@ const DragAndDrop = () => {
     return <div>
         <h1>Drag and drop</h1>
         <p>Experiment trying to implement simle drag and drop feature</p>
-        <MovableElem/>
+        <p>Draging is implemented by using <b>top</b> and <b>left</b> css properties,
+            which are being overwritten in mouse listeners on the div.
+            The listeners are MouseDown to start the draging, mouseMove perform the drag and mouseUp to end the dragging
+        </p>
+        <p>It currently has one drawback, which you can experience if you try to drag one block through another</p>
+        <MovableElem background="#28E9F1" initialPosition={[400, 250]}/>
+        <MovableElem background="#3FDF32" initialPosition={[75, 400]}/>
+        <MovableElem background="#F42CB1" initialPosition={[700, 600]}/>
     </div>
 };
 
@@ -16,13 +23,18 @@ interface MovableElemState {
     isDragged: boolean
 }
 
-class MovableElem extends React.Component<{}, MovableElemState> {
+interface MovableElemProps {
+    background: string
+    initialPosition: [number, number]
+}
 
-    constructor(props: any) {
+class MovableElem extends React.Component<MovableElemProps, MovableElemState> {
+
+    constructor(props: MovableElemProps) {
         super(props);
         this.state = {
-            currentTopLeftCornerPosition: [500, 250],
-            previousMousePosition: [500, 250],
+            currentTopLeftCornerPosition: props.initialPosition,
+            previousMousePosition: props.initialPosition,
             isDragged: false
         };
     }
@@ -60,7 +72,8 @@ class MovableElem extends React.Component<{}, MovableElemState> {
 
     getPositionAsCssStyle = () => ({
         left: this.state.currentTopLeftCornerPosition[0] + 'px',
-        top: this.state.currentTopLeftCornerPosition[1] + 'px'
+        top: this.state.currentTopLeftCornerPosition[1] + 'px',
+        background: this.props.background
     });
 
     render() {
@@ -70,8 +83,7 @@ class MovableElem extends React.Component<{}, MovableElemState> {
             onMouseMove={this.handleMouseMove}
             style={this.getPositionAsCssStyle()}
             className="movable-elem">
-            <h1>Elem</h1>
-            <p>Drag me around :)</p>
+            <h3>Drag me around :)</h3>
         </div>;
     }
 
